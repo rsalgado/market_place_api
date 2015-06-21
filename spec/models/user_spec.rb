eaf05d567d60d79c13f0e4c211edd40ffc6d28f7
Roberto Salgado <rsalgado03@gmail.com>
2015-06-21 18:31:03 -0500

    Refactor user specs with shoulda

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,8 @@ describe User do
 
   subject { @user }
 
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-
-  it { should be_valid }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
 end
