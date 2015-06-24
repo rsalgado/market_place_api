140953edd2bf79c878eb4a7f87a619e89dc9b9c3
Roberto Salgado <rsalgado03@gmail.com>
2015-06-24 17:53:43 -0500

    Minor fix in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :auth_token, uniqueness: true
-
+  # Callbacks
   before_create :generate_authentication_token!
+  # Validations
+  validates :auth_token, uniqueness: true
 
 
   def generate_authentication_token!
     begin
       self.auth_token = Devise.friendly_token
-    end while self.class.exists?(auth_token: auth_token)
+    end while self.class.exists?(auth_token: auth_token())
   end
 end
