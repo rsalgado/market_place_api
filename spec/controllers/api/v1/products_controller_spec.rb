82ccb727582c92f8be13198361037ad0303acfb3
Roberto Salgado <rsalgado03@gmail.com>
2015-06-25 11:46:31 -0500

    Create the products controller and implement its show and index actions (with corresponding specs and routes)

---
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Api::V1::ProductsController do
+  describe "GET #show" do
+    before(:each) do
+      @product = FactoryGirl.create(:product)
+      get :show, { id: @product.id }
+    end
+
+    it { should respond_with 200 }
+
+    it "returns the information about a reporter on a hash" do
+      product_response = json_response
+      expect(product_response[:title]).to eql @product.title
+    end
+  end
+
+  describe "GET #index" do
+    before(:each) do
+      4.times { FactoryGirl.create(:product) }
+      get :index
+    end
+
+    it { should respond_with 200 }
+
+    it "returns 4 records from the database" do
+      products_response = json_response
+      expect(products_response[:products]).to have(4).items
+    end
+  end
+end
