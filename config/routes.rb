893497ef7a8f4140f3d065366afd7ef493e194fd
Roberto Salgado <rsalgado03@gmail.com>
2015-06-28 18:13:54 -0500

    Add the show action for the order

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
           constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy] do
         resources :products, only: [:create, :update, :destroy]
-        resources :orders, only: [:index]
+        resources :orders, only: [:index, :show]
       end
       resources :sessions, only: [:create, :destroy]
       resources :products, only: [:show, :index]
--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -17,4 +17,20 @@ describe Api::V1::OrdersController do
       expect(order_response.size).to eql 4
     end
   end
+
+  describe "GET #show" do
+    before(:each) do
+      current_user = FactoryGirl.create :user
+      api_authorization_header(current_user.auth_token)
+      @order = FactoryGirl.create(:order, user: current_user)
+      get :show, { user_id: current_user.id, id: @order.id }      
+    end
+
+    it { should respond_with 200 }
+
+    it "returns the user order record matching the id" do
+      order_response = json_response[:order]
+      expect(order_response[:id]).to eql @order.id
+    end
+  end
 end
