4a723397a6dc9511bbc2e5c74e14ec41ce5994d0
Roberto Salgado <rsalgado03@gmail.com>
2015-06-27 19:33:06 -0500

    Fix issue with serialization

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,8 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :title, :price, :published, :user
+
+  def user
+    # This picks the UserSerializer attributes WITHOUT the associations, avoiding the repetition or circular references
+    UserSerializer.new(object.user).attributes
+  end
 end
