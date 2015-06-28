1db23abdf819575637ecaed449971aece4f79942
Roberto Salgado <rsalgado03@gmail.com>
2015-06-27 20:06:58 -0500

    Add search scopes on the product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,5 +16,57 @@ describe Product do
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   it { should validate_presence_of :user_id }
 
-  it { should belong_to :user }  
+  it { should belong_to :user }
+
+  describe ".filter_by_title" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, title: "A plasma TV"
+      @product2 = FactoryGirl.create :product, title: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, title: "CD player"
+      @product4 = FactoryGirl.create :product, title: "LCD TV"
+    end
+
+    context "when a 'TV' title pattern is sent" do
+      it "returns the 2 products matching" do
+        expect(Product.filter_by_title("TV").size).to eql(2)
+      end
+
+      it "returns the products matching" do
+        expect(Product.filter_by_title("TV").sort).to match_array([@product1, @product4])
+      end
+    end
+  end
+
+  context "filter by price or date" do
+    before(:each) do
+      @product1 = FactoryGirl.create(:product, price: 100)
+      @product2 = FactoryGirl.create(:product, price: 50)
+      @product3 = FactoryGirl.create(:product, price: 150)
+      @product4 = FactoryGirl.create(:product, price: 99)
+    end
+
+    describe ".above_or_equal_to_price" do    
+      it "returns the products which are aobve or equal to the price" do
+        expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3])
+      end
+    end
+
+    describe ".below_or_equal_to_price" do
+      it "returns the products which are below or equal to price" do
+        expect(Product.below_or_equal_to_price(99).sort).to match_array([@product2, @product4])
+      end
+    end
+
+    describe ".recent" do
+      before(:each) do
+        @product2.touch
+        @product3.touch
+      end
+
+      it "returns the most updated records" do
+        expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
+      end
+    end
+  end
+
 end
