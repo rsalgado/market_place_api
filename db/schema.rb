467061b9e760c4c1af9187b0623a4dcf908dc827
Roberto Salgado <rsalgado03@gmail.com>
2015-06-30 12:29:04 -0500

    Add quantity to placements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630153429) do
+ActiveRecord::Schema.define(version: 20150630172140) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20150630153429) do
   create_table "placements", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 0
   end
 
   add_index "placements", ["order_id"], name: "index_placements_on_order_id"
