a9dadf7cbc021916b5b3d071badfd1bf765b15d2
Roberto Salgado <rsalgado03@gmail.com>
2015-06-30 12:36:21 -0500

    Decrement the product quantity by the placement quantity

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,13 @@
 class Placement < ActiveRecord::Base
+  # Associations
   belongs_to :order, inverse_of: :placements
   belongs_to :product, inverse_of: :placements
+
+  # Callbacks
+  after_create :decrement_product_quantity!
+
+
+  def decrement_product_quantity!
+    self.product.decrement!(:quantity, quantity)
+  end
 end
